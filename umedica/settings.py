33524b5378866e45458a8525b0f6e194075c7bf9
Marlon Julian Perez Puentes <julianpuentes25@gmail.com>
2025-10-30 08:22:12 -0500

    arreglos

--- umedica/settings.py
+++ umedica/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xl%ta=j_n1jp8x%jv-ex%4s8b5oi23nn(aiq0@@a_1_(gkafc%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
