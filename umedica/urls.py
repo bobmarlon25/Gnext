48b3befcf06e1e0cd5386612354da56009df3bb5
Marlon Julian Perez Puentes <julianpuentes25@gmail.com>
2025-10-30 08:02:16 -0500

    arreglos

--- umedica/urls.py
+++ umedica/urls.py
@@ -18,9 +18,9 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
+    path('/admin/', admin.site.urls),
+    path('/account/', include('account.urls')),
     path('', include('inicio.urls')),
-    path('flota/', include('flota.urls'))
+    path('/flota/', include('flota.urls'))
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
